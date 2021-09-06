5b394b02ec3813bb5c55c06ee017e8f2f77c57e9
210904 <89740688+210904@users.noreply.github.com>
2021-09-06 21:54:26 +0530

    Create Display.c

---
+++ 3_Implementation/src/Display.c
@@ -0,0 +1,14 @@
+#include "Sorting.h"
+/**
+ * @brief Display function, to print the elements
+ * 
+ * @param array_ptr 
+ * @param array_size 
+ */
+void Display(int array_ptr[], int array_size)
+{
+    for(int i=0;i<array_size;i++)
+     printf("%d ",array_ptr[i]);
+    printf("\n");
+}
+© 2021 GitHub, Inc.
